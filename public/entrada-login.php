7e6668f55238f5009df9a6c3093573eb9a202b68
daviDsoareSS <david.silva425@etec.sp.gov.br>
2022-09-05 17:21:56 -0300

    alteração no index.html e adição das paginas .php 05.08.2022

---
+++ public/entrada-login.php
@@ -0,0 +1,85 @@
+<?php
+    include("conexao.php");
+    
+    if(isset($_POST['email']) || isset($_POST['senha'])){
+        if(strlen($_POST['email'])== 0){
+            echo "<p>Preencha o campo email.</p>";
+        }else if(strlen($_POST['senha'])== 0){
+                echo "<p>Preencha o campo senha.</p>";
+        }
+    else{
+        $email = mysqli_real_escape_string($conn,trim($_POST['email']));
+        $senha = mysqli_real_escape_string($conn,trim(base64_encode($_POST['senha'])));
+        
+        $sql = "SELECT * FROM users WHERE email = '$email' AND senha= '$senha'";
+        $result = $conn->query($sql) or die("Falha ao conectar: ". $conn->error);
+
+        $quantidade = $result->num_rows;
+
+        if($quantidade == 1){ 
+            $user = $result->fetch_assoc();
+            
+            if(!isset($_SESSION)){
+                session_start();
+            }
+            $_SESSION['id'] = $user['idUser'];
+            $_SESSION['nome'] = $user['nome'];
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['dataNasc'] = $user['dataNasc'];
+            $_SESSION['sexoUser'] = $user['sexoUser'];
+
+            header("Location: index.php");
+        
+        }else {
+            echo "Falha no login , verifique as informações inseridas.";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+    /*$email=$_POST['email'];
+    $senha=$_POST['senha'];
+    $_SESSION['email'] = $email;
+    //$senha=base64_encode($senha);
+    if ($conn->connect_error) {
+        die("Falha na conexão " . $conn->connect_error);
+    }
+
+    $sql = "SELECT email, senha FROM users;";
+    $dadosUser = "SELECT email , nome , dataNasc , pesoIni, altura , sexo FROM users;";
+    $result = $conn->query($sql);  // aqui
+    $logged = false;
+    while($row = $result->fetch_assoc()) {
+
+        if($row['email']==$email && $row['senha']==$senha){
+            header("location:index.php");   
+            $logged = true;
+            $_SESSION['email'] = $email;
+        }  
+        else
+            echo "Usuário não encontrado!";
+    }
+
+    if(isset($_SESSION['email'])){
+        if($_SESSION['email']!=""){
+            $logged = true;
+        }
+    }
+    if(isset($_GET['sair'])){
+        $_SESSION['email']="";
+        $logged = false;
+        unset($_SESSION['email']);
+        session_destroy();
+        header('login.html');
+        
+    }
+    $conn->close();
+
+*/
+?>
---
+++ public/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NutriAção -Login</title>
+    <link rel="icon" type="image/png" sizes="16x16"  href="img/favicon/favicon-16x16.png">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="css/styles.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Hammersmith+One&family=Inter:wght@700&family=Lato&display=swap');
+    </style>
+</head>
+<body id="pag-login">
+    <form class="container-login" action="entrada-login.php" method="post">
+        <div class="top-login">
+        <h1>LOGIN</h1>
+        <small>Email:</small>
+        <input type="text" name="email" id="" required>
+        <small>Senha:</small>
+        <input type="password" name="senha" required>
+        <a href="index.html"><button id="entrar" type="submit">ENTRAR</button></a>
+        <a href="recuperar-senha.php\"><h3 id="recuperar-senha">Esqueceu a senha?</h3></a>
+        </div>
+        <div class="criarConta">
+            <h3>É novo por aqui?</h3>
+            <a href="create-account.php"><p id="criar-conta">CRIAR CONTA</p></a>
+        </div>
+    </form>
+</body>
+</html>

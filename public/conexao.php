31440fee84b884a5e878dd386ce60f51e7080aa1
daviDsoareSS <david.silva425@etec.sp.gov.br>
2022-09-12 16:52:36 -0300

    banco de dados relacionado com sucesso 12.09.2022

--- public/conexao.php
+++ public/conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "forHouse";
+$servername = "us-cdbr-east-06.cleardb.net";
+$username = "b503ee8da3dc11";
+$password = "8aea5597";
+$database = "heroku_acd6119eb0eb682";
 
 $conn = mysqli_connect($servername, $username, $password, $database) or die("Falha ao conectar ao banco de dados: ".$conn->error) ;
 

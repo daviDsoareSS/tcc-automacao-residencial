623dab1282a256df0feaff06103f0e6bea89c3f6
daviDsoareSS <david.silva425@etec.sp.gov.br>
2022-09-13 11:31:59 -0300

    chatbot adicionado 13.09.2022

---
+++ public/service.php
@@ -0,0 +1,23 @@
+<?php
+    include "includes/nav-bar.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!--CRISP CHATBOT-->
+    <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="ba3d4d34-ed87-48b9-9a7d-2d7f532b4699";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+    <!--FIM CRISP CHATBOT-->
+</head>
+<body>
+    <button onclick="$crisp.push(['do', 'session:reset']);";>Clique em mim</button>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic perspiciatis quo praesentium consectetur voluptatum veritatis, qui doloribus alias quasi voluptate similique eligendi, exercitationem accusantium incidunt corrupti excepturi minima ab officia.</p>
+<?php
+    
+?>
+</body>
+</html>

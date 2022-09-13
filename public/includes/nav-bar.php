17dae9e305b883b512c63c381456cd06ebe128b9
daviDsoareSS <david.silva425@etec.sp.gov.br>
2022-09-13 13:25:13 -0300

    tela preloader adicionada 13.09.2022

--- public/includes/nav-bar.php
+++ public/includes/nav-bar.php
@@ -43,7 +43,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="index.html">Inicio</a>
+              <a class="nav-link" href="index.php">Inicio</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#servicos">Serviços</a>

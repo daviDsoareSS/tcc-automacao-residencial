e2ad3682d4b0ba24861344903aca83ed47a010ec
daviDsoareSS <david.silva425@etec.sp.gov.br>
2022-10-08 20:21:14 -0300

    criação das paginas de serviços 08.10.2022

--- public/perfil.php
+++ public/perfil.php
@@ -27,10 +27,18 @@
         <div class="container-dados-pessoais">
             <h2><span></span>Dados pessoais</h2>
             <ul>
-                <li>Email:<span class="dados-user">thelordsofgames6@gmail.com</span></li>
-                <li>Nome completo:<span class="dados-user">David Soares Silva</span></li>
-                <li>Idade:<span class="dados-user">18 anos</span></li>
-                <li>Sexo:<span class="dados-user">Masculino</span></li>
+                <li>Email:<span class="dados-user"><?php echo($_SESSION['email']);?></span></li>
+                <li>Nome completo:<span class="dados-user"><?php echo($_SESSION['nome']);?></span></li>
+                <!--Função para calcular idade do usuário-->
+                <?php
+                 //FUNÇÃO CALCULAR IDADE DO USUARIO
+                  $dataNascimento = $_SESSION['dataNasc'];
+                  $data = new DateTime($dataNascimento);
+                  $resultado = $data->diff(new DateTime( date('Y-m-d')));
+                 //FIM FUNÇÃO
+                ?>
+                <li>Idade:<span class="dados-user"><?php echo $resultado->format( '%Y anos' );?></span></li>
+                <li>Sexo:<span class="dados-user"><?php echo($_SESSION['sexoUser']);?></span></li>
                 <li>Cidade:<span class="dados-user">São Paulo -SP</span></li>
                 <li>Endereço:<span class="dados-user endereco">Rua Recife,Vila Seabra Nº30 - 08180-425</span></li>
                 <li></li>
--- public/portao-eletrico.php
+++ public/portao-eletrico.php
@@ -6,12 +6,56 @@
   //NAV-BAR
   include 'includes/nav-bar.php';
 ?>
-<title>Portão Elétrico | 4House</title>
-<body>
+<title>Sensor de proximidade | 4House</title>
+<body class="pag-portao-eletrico">
+<!-- início do preloader -->
+  <div id="preloader">
+    <div class="inner">
+       <!-- HTML DA ANIMAÇÃO MUITO LOUCA DO SEU PRELOADER! -->
+       <img src="img/gif/1490.gif" alt="">
+    </div>
+</div>
+<!-- fim do preloader --> 
+<div class="modal">
+</div>
+  <span class="circle">
+    <svg height="52vw" width="10vw"></svg>
+  </span>
+<main>
+    <img src="img/servicos/portao-eletrico/main/portao-eletrico.png" alt="">
+    <div class="container-text-portao-eletrico">
+        <h1>Portão elétrico</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit voluptas excepturi facere necessitatibus iusto rerum laboriosam soluta aliquam maiores eum, voluptatum optio sapiente, asperiores veritatis vitae ullam animi totam suscipit?
+        Optio cupiditate nobis labore, numquam necessitatibus recusandae sed, laboriosam quaerat illo dolore est! Consequuntur, reiciendis laborum libero architecto alias numquam ipsum minus laboriosam cumque? Accusantium aperiam cupiditate iure sit rem?</p>
+        <div class="cards-portao-eletrico">
+            <div class="cards">
+                <img src="img/servicos/sensor-proximidade/main/seguranca.png" alt="">
+                <p>Nivel de segurança</p>
+                <small>Alto nível</small>
+            </div> 
+            <div class="cards">
+                <img src="img/servicos/sensor-proximidade/main/campeao.png" alt="">
+                <p>Campeão</p>
+                <small>Serviço mais contratado</small>
+            </div>    
+            <div class="cards">
+                <img src="img/servicos/sensor-proximidade/main/tempo.png" alt="">
+                <p>Instalação</p>
+                <small>Tempo médio de 4 horas</small>
+            </div>       
+            <div class="cards">
+                <img src="img/servicos/sensor-proximidade/main/seguranca.png" alt="">
+                <p>Nivel de segurança</p>
+                <small>Alto nível</small>
+            </div>    
+        </div>
+        <button id="contrate-servico">Contrate agora</button>
+    </div>
+</main>
 
 <?php
     //FOOTER
-    //include 'includes/footer.php';
+    include 'includes/footer.php';
 ?>
 </body>
 </html>

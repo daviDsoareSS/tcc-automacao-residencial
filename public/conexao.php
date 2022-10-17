480fe82493f5831614a5db0620a2f6cc110700cb
GuilhermeLeodeOliveira <97077427+GuilhermeLeodeOliveira@users.noreply.github.com>
2022-10-17 17:11:51 -0300

    Update conexao.php

--- public/conexao.php
+++ public/conexao.php
@@ -1,12 +1,13 @@
 <?php
-/*
+
 $servername = "us-cdbr-east-06.cleardb.net";
 $username = "b503ee8da3dc11";
 $password = "8aea5597";
 $database = "heroku_acd6119eb0eb682";
 
 $conn = mysqli_connect($servername, $username, $password, $database) or die("Falha ao conectar ao banco de dados: ".$conn->error) ;
-*/
+
+/*
 
 $servername = "localhost";
 $username = "root";
@@ -14,7 +15,7 @@ $password = "";
 $database = "forhouse";
 
 $conn = mysqli_connect($servername, $username, $password, $database) or die("Falha ao conectar ao banco de dados: ".$conn->error) ;
-
+*/
 
 
 ?>

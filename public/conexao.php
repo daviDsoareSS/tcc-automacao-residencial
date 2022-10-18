8c76bcab9286696f1b6665d4557cd49b70dd0346
daviDsoareSS <david.silva425@etec.sp.gov.br>
2022-10-18 01:38:25 -0300

    alterei o arquivo conexão

--- public/conexao.php
+++ public/conexao.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 $servername = "us-cdbr-east-06.cleardb.net";
 $username = "b503ee8da3dc11";
 $password = "8aea5597";
@@ -7,12 +7,12 @@ $database = "heroku_acd6119eb0eb682";
 
 $conn = mysqli_connect($servername, $username, $password, $database) or die("Falha ao conectar ao banco de dados: ".$conn->error) ;
 
-*/
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "forhouse";
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "forhouse";
 
 $conn = mysqli_connect($servername, $username, $password, $database) or die("Falha ao conectar ao banco de dados: ".$conn->error) ;
 

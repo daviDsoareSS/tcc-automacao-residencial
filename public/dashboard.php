ec965a21baf9cb6621d4a56d529eb9b67eae8236
GuilhermeLeodeOliveira <97077427+GuilhermeLeodeOliveira@users.noreply.github.com>
2022-10-29 00:11:24 -0300

    terminei a parte de seleção dos usuários

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,17 +1,23 @@
-
 <?php
+
     session_start();
 
     include_once('conexao.php');
     
+    $filtro = $_GET['filtro'];
 
-    if(!empty($_GET['search']))
-    {
-        $data = $_GET['search'];
-        $sql = "SELECT * FROM  users WHERE idUser LIKE '%$data%' or nome LIKE '%$data%' or email LIKE '%$data%' ORDER BY idUser DESC";
+    if(empty($_GET['filtro'])){
+        $filtro = "DESC";
     }
-    else{
-        $sql = "SELECT * FROM  users ORDER BY idUser DESC";
+
+    if(!empty($_GET['search'])){
+
+        $data = $_GET['search'];
+        
+        $sql = "SELECT * FROM  users WHERE idUser LIKE '%$data%' or nome LIKE '%$data%' or email LIKE '%$data%' ORDER BY idUser $filtro";
+
+    }else{
+        $sql = "SELECT * FROM  users ORDER BY idUser $filtro";
     }
    
 
@@ -63,14 +69,14 @@
     </div>
     <main>
         <div class="top-main">      
-              <input type="search" class="form-control" id="pesquisa-usuarios" placeholder="Pesquisar usuário...">  
-              <button onclick="searchData()">Procurar</button>    
-              <label for="option">Filtrar</label>
-              <select name="filtro" id="filtro">
-                <option value="todos-usuarios">Todos</option>
-                <option value="ordem-crescente">Ordem crescente</option>
-                <option value="ordem-decrescente">Ordem decrescente</option>
-              </select>    
+            <input type="search" class="form-control" id="pesquisa-usuarios" placeholder="Pesquisar usuário...">  
+            <button onclick="searchData()">Procurar</button>    
+            <label for="option">Filtrar</label>
+            <select name="filtro" id="filtro">
+                <option value="DESC">Todos</option>
+                <option value="ASC">Ordem crescente</option>
+                <option value="DESC">Ordem decrescente</option>
+            </select>    
         </div>
         <div class="container-main">
             <table border="1">
@@ -103,10 +109,11 @@
              </table>
         </div>
     </main>
+
     <script>
         /*SISTEMA DE BUSCA NO DASHBOARD*/
         const search = document.getElementById('pesquisa-usuarios');
-        
+        const filtro = document.getElementById('filtro');
         search.addEventListener("keydown", function(event){
             if(event.key === "Enter"){
                 searchData();
@@ -114,7 +121,12 @@
         });
 
         function searchData(){   
-            window.location = 'dashboard.php?search='+ search.value;
+            
+
+            opcaoValor = filtro.options[filtro.selectedIndex].value;
+    
+            window.location = 'dashboard.php?search='+search.value +'&filtro=' + opcaoValor;
+            
         }
     </script>
 </body>

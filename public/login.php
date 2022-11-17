1fcd5e71f4b5826e1bc2ced5018cedcce794eba9
daviDsoareSS <david.silva425@etec.sp.gov.br>
2022-11-17 20:17:06 +0000

    atualizei

--- public/login.php
+++ public/login.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | 4House</title>
+    <title>| 4House</title>
     <link rel="icon" type="image/png" sizes="16x16"  href="img/favicon/favicon-16x16.png">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="theme-color" content="#ffffff">

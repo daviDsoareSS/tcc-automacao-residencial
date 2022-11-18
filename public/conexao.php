589695825f54cf08f35f3197e37f04f6d6fd39db
daviDsoareSS <david.silva425@etec.sp.gov.br>
2022-11-18 09:52:09 -0300

    atualizei o arquivo conexao

--- public/conexao.php
+++ public/conexao.php
@@ -6,6 +6,7 @@ $password = "David12304$";
 $database = "fourhouse_db";
 
 $conn = mysqli_connect($servername, $username, $password, $database) or die("Falha ao conectar ao banco de dados: ".$conn->error) ;
+mysqli_set_charset($conn,'utf8mb4');
 // $servername = "localhost";
 // $username = "root";
 // $password = "";

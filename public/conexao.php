2730075be60dbb855d47fd6b0ea14bbedca53745
GuilhermeLeodeOliveira <go507744@gmail.com>
2022-11-19 00:23:15 -0300

    Update conexao.php

--- public/conexao.php
+++ public/conexao.php
@@ -1,28 +1,16 @@
 <?php
 
-define( 'MYSQL_HOST', 'fourhouse-db.ceplxgrj0xpi.us-east-1.rds.amazonaws.com' );
-define( 'MYSQL_USER', 'daviDsoareSS' );
-define( 'MYSQL_PASSWORD', 'David12304' );
-define( 'MYSQL_DB_NAME', 'fourhouse_db' );
-
-
-try
-{
-    $PDO = new PDO( 'mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD );
-}
-catch ( PDOException $e )
-{
-    echo 'Erro ao conectar com o MySQL: ' . $e->getMessage();
-}
-
-
-/*
 $servername = "fourhouse-db.ceplxgrj0xpi.us-east-1.rds.amazonaws.com";
 $username = "daviDsoareSS";
 $password = "David12304$";
 $database = "fourhouse_db";
 
 $conn = mysqli_connect($servername, $username, $password, $database) or die("Falha ao conectar ao banco de dados: ".$conn->error) ;
-*/
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "forhouse";
+
 
 ?>

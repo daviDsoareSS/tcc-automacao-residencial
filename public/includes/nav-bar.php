b14c8dd8361523254c2ae749ab22cd8afa3b2fb8
daviDsoareSS <david.silva425@etec.sp.gov.br>
2022-11-19 18:55:35 -0300

    input de pesquisa finalizado 19.11.2022

--- public/includes/nav-bar.php
+++ public/includes/nav-bar.php
@@ -9,6 +9,8 @@
     $sqlServices = "SELECT * FROM  servico WHERE nomeServico LIKE '%$data%' or urlServico LIKE '%$data%'";
   
     $resultServices = $conn->query($sqlServices);
+
+    $num_rows_services = $resultServices->num_rows;
   }
 
   if(!isset($_SESSION['idUser'])){
@@ -183,20 +185,25 @@
       </nav>
       <?php 
        if(!empty($_GET['search'])){
-        echo "<div class='container-modal-services'>";
+        echo "<div class='container-modal-services' id='modal'>";
           echo "<div class='modal-services'>";
             echo "<h2>Resultados para <i>'$data'</i></h2>";
-            echo "<span>X</span>";
+            echo "<span onclick='fechar()'><svg clip-rule='evenodd' fill-rule='evenodd' stroke-linejoin='round' stroke-miterlimit='2' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path d='m12.002 2.005c5.518 0 9.998 4.48 9.998 9.997 0 5.518-4.48 9.998-9.998 9.998-5.517 0-9.997-4.48-9.997-9.998 0-5.517 4.48-9.997 9.997-9.997zm0 8.933-2.721-2.722c-.146-.146-.339-.219-.531-.219-.404 0-.75.324-.75.749 0 .193.073.384.219.531l2.722 2.722-2.728 2.728c-.147.147-.22.34-.22.531 0 .427.35.75.751.75.192 0 .384-.073.53-.219l2.728-2.728 2.729 2.728c.146.146.338.219.53.219.401 0 .75-.323.75-.75 0-.191-.073-.384-.22-.531l-2.727-2.728 2.717-2.717c.146-.147.219-.338.219-.531 0-.425-.346-.75-.75-.75-.192 0-.385.073-.531.22z' fill-rule='nonzero'/></svg></span>";
           while($services_data = mysqli_fetch_assoc($resultServices)){
-                  echo "<a href='$services_data[urlServico].php'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M13.723 18.654l-3.61 3.609c-2.316 2.315-6.063 2.315-8.378 0-1.12-1.118-1.735-2.606-1.735-4.188 0-1.582.615-3.07 1.734-4.189l4.866-4.865c2.355-2.355 6.114-2.262 8.377 0 .453.453.81.973 1.089 1.527l-1.593 1.592c-.18-.613-.5-1.189-.964-1.652-1.448-1.448-3.93-1.51-5.439-.001l-.001.002-4.867 4.865c-1.5 1.499-1.5 3.941 0 5.44 1.517 1.517 3.958 1.488 5.442 0l2.425-2.424c.993.284 1.791.335 2.654.284zm.161-16.918l-3.574 3.576c.847-.05 1.655 0 2.653.283l2.393-2.389c1.498-1.502 3.94-1.5 5.44-.001 1.517 1.518 1.486 3.959 0 5.442l-4.831 4.831-.003.002c-1.438 1.437-3.886 1.552-5.439-.002-.473-.474-.785-1.042-.956-1.643l-.084.068-1.517 1.515c.28.556.635 1.075 1.088 1.528 2.245 2.245 6.004 2.374 8.378 0l4.832-4.831c2.314-2.316 2.316-6.062-.001-8.377-2.317-2.321-6.067-2.313-8.379-.002z'/></svg><p>".$services_data['nomeServico']."</p></a>";             
+                  echo "<a href='$services_data[urlServico].php'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M13.723 18.654l-3.61 3.609c-2.316 2.315-6.063 2.315-8.378 0-1.12-1.118-1.735-2.606-1.735-4.188 0-1.582.615-3.07 1.734-4.189l4.866-4.865c2.355-2.355 6.114-2.262 8.377 0 .453.453.81.973 1.089 1.527l-1.593 1.592c-.18-.613-.5-1.189-.964-1.652-1.448-1.448-3.93-1.51-5.439-.001l-.001.002-4.867 4.865c-1.5 1.499-1.5 3.941 0 5.44 1.517 1.517 3.958 1.488 5.442 0l2.425-2.424c.993.284 1.791.335 2.654.284zm.161-16.918l-3.574 3.576c.847-.05 1.655 0 2.653.283l2.393-2.389c1.498-1.502 3.94-1.5 5.44-.001 1.517 1.518 1.486 3.959 0 5.442l-4.831 4.831-.003.002c-1.438 1.437-3.886 1.552-5.439-.002-.473-.474-.785-1.042-.956-1.643l-.084.068-1.517 1.515c.28.556.635 1.075 1.088 1.528 2.245 2.245 6.004 2.374 8.378 0l4.832-4.831c2.314-2.316 2.316-6.062-.001-8.377-2.317-2.321-6.067-2.313-8.379-.002z'/></svg><p>".$services_data['nomeServico']."</p></a>";                              
+                }
+                if($num_rows_services == 0){
+                  echo "<p>Lamentamos, nenhum serviço encontrado com esse critério de pesquisa.</p>";
                 }
               }
+              
           echo "</div>";     
         echo "</div>";     
       ?>
       <script>
         /*SISTEMA DE BUSCA NO DASHBOARD*/
         const search = document.getElementById('pesquisa-servicos');
+        
         search.addEventListener("keydown", function(event){
             if(event.key === "Enter"){
                 searchData();
@@ -205,6 +212,11 @@
 
         function searchData(){   
             window.location = 'index.php?search='+search.value;
+        }
+        const modal = document.getElementById('modal');
+
+        function fechar(){
+          modal.classList.add("fechar");
         }
       </script>
 </body>

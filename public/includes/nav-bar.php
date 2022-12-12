3802d213530163df03fcd44619d3ed4dcbd5b3f6
ThiagoMaritns11 <thiago.marques29@etec.sp.gov.br>
2022-12-12 20:49:09 -0300

    index atualizado

--- public/includes/nav-bar.php
+++ public/includes/nav-bar.php
@@ -78,7 +78,7 @@
     <!--FIM FAVICON-->
     <?php
       if(isset($_SESSION['idUser'])){
-        echo "<script type='text/javascript'>window.$crisp=[];window.CRISP_WEBSITE_ID='5f0a251c-2746-4d3e-90a8-0893be62345e';(function(){d=document;s=d.createElement('script');s.src='https://client.crisp.chat/l.js';s.async=1;d.getElementsByTagName('head')[0].appendChild(s);})();</script>"
+        echo "<script type='text/javascript'>window.$crisp=[];window.CRISP_WEBSITE_ID='5f0a251c-2746-4d3e-90a8-0893be62345e';(function(){d=document;s=d.createElement('script');s.src='https://client.crisp.chat/l.js';s.async=1;d.getElementsByTagName('head')[0].appendChild(s);})();</script>";
       }
     
     ?>
